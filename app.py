f0f227a7d5d16c99250cb147ce70e487dea20225
Pavithra <pavithrac.offl@gmail.com>
2026-09-17 19:42:45 +0530

    Add files via upload
    UI commit

---
+++ app.py
@@ -0,0 +1,78 @@
+import pandas as pd
+import streamlit as st
+import plotly.express as px
+from PIL import Image
+
+st.set_page_config(page_title='Customer review dashboard')
+st.title('Customer review dashboard')
+st.subheader('Use slider to see the review result based on the age')
+
+
+### Load dataframe
+excel_file = 'project.xlsx'
+sheet_name = 'DATA'
+
+
+df = pd.read_excel(excel_file,
+                   sheet_name=sheet_name,
+                   usecols='B:D',
+                   header=3)
+
+df_users = pd.read_excel(excel_file,
+                         sheet_name = sheet_name,
+                         usecols="G:H",
+                         header=3)
+
+
+#streamlit selection
+devices = df["Devices"]. unique().tolist()
+ages = df['Age'].unique().tolist()
+
+age_selection = st.slider('Age:',
+                          min_value=min(ages),
+                          max_value=max(ages),
+                          value=(min(ages),max(ages)))
+
+devices_selection = st.multiselect('Devices:',
+                                   devices,
+                                   default=devices)
+
+#filter dataframe based on selection
+mask = (df['Age'].between(*age_selection)) & (df['Devices'].isin(devices_selection))
+number_of_result = df[mask].shape[0]
+st.markdown(f'*Available results:{number_of_result}*')
+
+
+#group dataframe after selection
+df_grouped = df[mask].groupby(by=['Users Rating']).count()[['Age']]
+df_grouped = df_grouped.rename(columns={'Age':'Votes'})
+df_grouped = df_grouped.reset_index()
+
+#plot bar chart
+bar_chart = px.bar(df_grouped,
+                   x='Users Rating',
+                   y='Votes',
+                   text='Votes',
+                   color_discrete_sequence=['#45B39D']*len(df_grouped),
+                   template='plotly_white')
+st.plotly_chart(bar_chart)
+
+#display image & dataframe
+col1, col2 = st.columns(2)
+image = Image.open('images/firmbee-com-jrh5lAq-mIs-unsplash.jpg')
+col1.image(image,
+         use_column_width=True)
+col2.dataframe(df[mask])
+
+#plot pie chart
+
+pie_chart = px.pie(df_users,
+                   title='Total no. of Users',
+                   values='Users',
+                   names='Device')
+
+st.plotly_chart(pie_chart)  
+
+image = Image.open('images/firmbee-com-jrh5lAq-mIs-unsplash.jpg')
+st.image(image,
+         use_column_width=True)
---
+++ readme.txt
@@ -0,0 +1,16 @@
+pplotly==4.14.3 -> charts
+streamlit==0.74.1 -> python UI
+pandas==1.2.0 -> dataframe
+pillow==8.1.0 -> Library
+
+system level installation:
+python
+
+python libraries installation:
+pip install plotly streamlit pandas pillow
+pip install openpyxl
+
+To run the project:
+streamlit run app.py
+
+
